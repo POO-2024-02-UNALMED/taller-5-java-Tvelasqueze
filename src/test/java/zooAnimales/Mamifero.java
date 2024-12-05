9db1da3e8469676a8cee5416bf8d4b7f258090f7
Tomas Velasquez <tvelasqueze@unal.edu.co>
2024-12-05 17:42:44 -0500

    prueba 1

---
+++ src/test/java/zooAnimales/Mamifero.java
@@ -0,0 +1,58 @@
+package zooAnimales;
+import java.util.ArrayList;
+
+public class Mamifero extends Animal{
+    private static ArrayList<Mamifero> listado = new ArrayList<Mamifero>();
+	public static int caballos;
+	public static int leones;
+	private boolean pelaje;
+	private int patas;
+    
+	//Constructores
+	
+	public Mamifero(String nombre, int edad, String habitat,String genero,boolean pelaje, int patas) {
+		super(nombre, edad, habitat, genero);
+		this.pelaje = pelaje;
+		this.patas = patas;
+		listado.add(this);
+	}	
+	
+	public Mamifero() {	
+		this(null,0,null,null,false,0);
+	}
+
+	//Set and Get
+
+	public boolean getPelaje() {
+		return pelaje;
+	}
+
+	public void setPelaje(boolean pelaje) {
+		this.pelaje = pelaje;
+	}
+
+	public int getPatas() {
+		return patas;
+	}
+
+	public void setPatas(int patas) {
+		this.patas = patas;
+	}
+
+	//Metodos
+
+	static public int cantidadMamiferos(){
+		return listado.size();
+	}
+
+	public static Mamifero crearCaballo(String nombre, int edad, String genero){
+		caballos ++;	
+		return new Mamifero(nombre, edad, "pradera", genero, true, 4); 	
+	}
+	
+	public static Mamifero crearLeon(String nombre, int edad, String genero) {
+		leones ++;
+		return new Mamifero(nombre, edad, "selva", genero, true, 4);
+	} 
+	
+}
